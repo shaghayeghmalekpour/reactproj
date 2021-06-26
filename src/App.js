5a45960b2728a53e690d1e2af1650be6829d925a
shaghayeghmalekpour <mk.shaqa@gmail.com>
2021-06-27 01:57:12 +0430

    a new react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+// import Butt from "./Butt.js";
 function App() {
+  const [close, setClose] = useState(false);
+
+  const change = () => {
+    setClose(true);
+ console.log("HI");
+  };
+  const open =()=>{
+ setClose(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className={`navBar ${close ? 'check' : 'uncheck'}`}
+      // style={{
+      //   width: "200px",
+      //   height: "100vh",
+      //   backgroundColor: "white",
+      //   float: "right",
+      //   zIndex: "1",
+      // }}
+      // id="navButt"
+      Hi this a Test For You
+    >
+      </div>
+      <div>
+     
+      <button id="btt"
+        onClick={change}
+      >
+        close
+      </button>
+      <button id="butt2">
+    {/* onClick={open} */}
+    Open
+      </button>
+      {/* <Butt isCheck={close} /> */}
     </div>
+    </>
   );
 }
 
 export default App;
+
+// const text="shaghayegh"
+// const [myText,setMyText]=useState("goodbye")
+// const ali =()=>{
+// setMyText("ali")
+// }
+// return (
+//   <>
+
+//     <div>
+//       {myText}
+//     </div>
+//     <div>hello again</div>
+//     <button style={{backgroundColor:"red", color:"white"}} onClick={ali}>
+//       change my text
+//     </button>
+//   </>
+// );
---
+++ src/Butt.js
@@ -0,0 +1,13 @@
+import React from "react"
+function Butt({isCheck }){
+return (
+    <div id="navButt" className={isCheck ? "check" : "uncheck"} >
+
+    </div>
+)
+}
+
+
+
+
+export default Butt;
